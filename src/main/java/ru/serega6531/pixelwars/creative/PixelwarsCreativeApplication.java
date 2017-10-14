d0daf53aec7766daa1af9daaba8154b95f4ef0bd
serega6531 <serega6531@yandex.ru>
2017-10-15 00:09:31 +0300

    Initial commit

---
+++ src/main/java/ru/serega6531/pixelwars/creative/PixelwarsCreativeApplication.java
@@ -0,0 +1,12 @@
+package ru.serega6531.pixelwars.creative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelwarsCreativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelwarsCreativeApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/serega6531/pixelwars/creative/PixelwarsCreativeApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.serega6531.pixelwars.creative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PixelwarsCreativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
